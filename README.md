439ba1a83192b12ac17b23cf1039d2f1c5d391c1
Jiwoo So <58384366+jiwooproity@users.noreply.github.com>
2025-12-07 16:06:56 +0900

    Core(create-flutter): - 프로젝트의 간단 소개, 목적을 위해 README.md 작성 - 프로젝트 생성 및 깃 초기화, 연결

---
+++ README.md
@@ -0,0 +1,4 @@
+# Cream Social Media APP
+Flutter 적응기를 위한 소셜 미디어 서비스 앱입니다.
+
+## 라이브러리
---
+++ android/app/src/main/kotlin/com/jiwoo/dev/cream_sns/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.jiwoo.dev.cream_sns
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
