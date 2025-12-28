13de4b60df7135eeea103357448803bcf9823282
Jiwoo So <58384366+jiwooproity@users.noreply.github.com>
2025-12-28 11:09:02 +0900

    Feat(alert-toast): Flutter toast provider 추가 ## Feat - 한번의 생성만 이루어지는 Toast provider 생성

--- lib/shared/toast/show_toast.dart
+++ lib/shared/toast/show_toast.dart
@@ -1,26 +1,31 @@
 import "package:flutter/material.dart";
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 
 // Themes
 import 'package:cream_sns/core/theme/app_colors.dart';
 
-class ShowToast {
-  ShowToast._internal();
+final toastProvider = Provider.family<Toast, BuildContext>((ref, context) {
+  final toast = Toast();
+  toast.init(context);
+  return toast;
+});
 
-  static final ShowToast _instance = ShowToast._internal();
+abstract class ToastImpl {
+  void show({required String message});
+}
 
-  factory ShowToast() => _instance;
+class Toast implements ToastImpl {
+  static final FToast _fToast = FToast();
 
-  final FToast _fToast = FToast();
-
-  void init(BuildContext context) {
+  void init(BuildContext context){
     _fToast.init(context);
   }
 
-  void show({
-    required String message,
-    Duration duration = const Duration(seconds: 2),
-  }) {
+  @override
+  void show({required String? message}) {
+    if (message == null) return;
+
     Widget toast = Container(
       padding: const EdgeInsets.all(15),
       decoration: BoxDecoration(
